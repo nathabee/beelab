2a141118bfb587dbc47132ad3fbabeb3fb645959
nathabee <nathalie.werner.fr@gmail.com>
2025-09-29 13:47:04 +0200

    wordpress plugin user exeternalized in shared add errormanagement page in pomolobee

--- wordpress/plugin-src/pomolobee/src/version.ts
+++ wordpress/plugin-src/pomolobee/src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION='v1.1.5'; export const COMMIT='89f50d1'; export const BUILT_AT='2025-09-29T11:04:23.195Z';
+export const VERSION='v1.1.5'; export const COMMIT='a7621fd'; export const BUILT_AT='2025-09-29T11:43:07.938Z';
