6ad3f764a952da405cbb73c08888cc64c75ed172
nathabee <nathalie.werner.fr@gmail.com>
2025-10-04 14:42:39 +0200

    chore(pomolobee): build artifacts

--- wordpress/plugin-src/pomolobee/pomolobee.php
+++ wordpress/plugin-src/pomolobee/pomolobee.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       PomoloBee WP
  * Description:       FSE blocks integrating with Django backend.
- * Version:           v1.1.7
+ * Version:           v1.1.8
  * Author:            Nathabee
  */
 
