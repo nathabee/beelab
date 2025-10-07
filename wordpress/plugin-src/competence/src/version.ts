bf7150eb897f0076a1ab35aaaed933254c623ef7
nathabee <nathalie.werner.fr@gmail.com>
2025-10-07 21:49:57 +0200

    competence translation and language

--- wordpress/plugin-src/competence/src/version.ts
+++ wordpress/plugin-src/competence/src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION='v1.1.9'; export const COMMIT='abf22d0'; export const BUILT_AT='2025-10-07T17:56:09.991Z';
+export const VERSION='v1.1.9'; export const COMMIT='05f5dbf'; export const BUILT_AT='2025-10-07T19:49:14.322Z';
--- wordpress/plugin-src/pomolobee/src/version.ts
+++ wordpress/plugin-src/pomolobee/src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION='v1.1.8'; export const COMMIT='8e46b95'; export const BUILT_AT='2025-10-04T12:42:33.654Z';
+export const VERSION='v1.1.9'; export const COMMIT='e6d47ab'; export const BUILT_AT='2025-10-07T19:49:30.245Z';
