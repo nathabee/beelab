b6fb75a4103b7525021167f6c5be6a567a68a821
Nathabee <nathalie.werner.fr@gmail.com>
2026-01-11 13:59:35 +0100

    add cgo-demo in web service to show an example of a  static web service. code orginal in chatgpt-organizer project

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: "/cgo-demo", destination: "/cgo-demo/index.html", permanent: false },
+      { source: "/cgo-demo/", destination: "/cgo-demo/index.html", permanent: false },
+    ];
+  },
+};
+
 export default nextConfig;
